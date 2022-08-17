ff10e1b11b3e89d75cc25f68b24559f57cfcdf0e
Ulia Kamalova <mysula@mail.ru>
2022-08-17 13:35:10 +0500

    19 ЗАДАЧА

---
+++ 19 Task/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int a = ReadInt("Написать первое число A: ");
+int b = ReadInt("Написать второе число B: ");
+int с = ReadInt("Написать третье число C: ");
+int d = ReadInt("Написать четвертое число D:");
+int e = ReadInt("Написать пятое число E: ");
+
+if(a == e && b == d) 
+{
+    Console.WriteLine("Является паллиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является паллиндромом");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
